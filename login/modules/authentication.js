1da511ba88208d48e8d749a959813eaf6e09134a
Judy Chen <time.fly.arrow@gmail.com>
2015-01-31 01:09:14 -0500

    include kik api

--- login/modules/authentication.js
+++ login/modules/authentication.js
@@ -1,10 +1,12 @@
+<script src="http://cdn.kik.com/kik/1.0.18/kik.js"></script>
+
 var anonToken;
 var host;
 var signedData;
 
 kik.anonymousVerify(anonToken, host, signedData, function(err, unsigned Data){
 	if(err){
-		
+
 	} else{
 
 	}
