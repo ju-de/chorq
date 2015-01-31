43c422d7abb7b6a801a27a33fe692b56166df7b9
Judy Chen <time.fly.arrow@gmail.com>
2015-01-31 03:19:29 -0500

    update kik functions

--- login/modules/authentication.js
+++ login/modules/authentication.js
@@ -1,14 +1,18 @@
 <script src="http://cdn.kik.com/kik/1.0.18/kik.js"></script>
 
+var kik = require('kik-lib');
 var anonToken;
 var host;
 var signedData;
 
-kik.anonymousVerify(anonToken, host, signedData, function(err, unsigned Data){
+kik.getAnonymousUser(function (token) {
+	typeof token; // 'string'
+});
+
+kik.verify(anonToken, host, signedData, function(err, unsignedData){
 	if(err){
 
 	} else{
 
 	}
-
-})
+});
