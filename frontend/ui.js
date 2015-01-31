80ebb8796efe028b7cb38501a9353433459744b1
James Yu <jj6yu@uwaterloo.ca>
2015-01-31 18:24:50 -0500

    more stuff
    top key

--- frontend/ui.js
+++ frontend/ui.js
@@ -69,6 +69,15 @@ function bigSearchBlur() {
         $("#bigsearchbar").val("Search for a song...");
 }
 
+function onFocus(obj,def) {
+    if(obj.val() == def)
+        obj.val("");
+}
+function onBlur(obj,def) {
+    if(obj.val() == "")
+        obj.val(def);
+}
+
 function toggleFiles() {
     if($("#files").hasClass("hidden"))
         showFiles();
@@ -88,7 +97,7 @@ function hideFiles() {
     if($("#stack").hasClass("hidden") && $("#searchresults").hasClass("hidden")) {
         $("#searchbar").addClass("hidden");
         $("#home").removeClass("hidden");
-        window.location.hash="#!/sheet:"+id;
+        window.location.hash="#!/home";
         setTitle("");
     }
 }
@@ -97,10 +106,8 @@ function hideFolders() {
     $("#folders").addClass("hidden");
 }
 
-function toggleFolders() {
-    if($("#folders").hasClass("hidden"))
-        $("#folders").removeClass("hidden");
-    else hideFolders();
+function showFolders() {
+    $("#folders").removeClass("hidden");
 }
 
 /*********************
@@ -147,6 +154,11 @@ function showSheet(id,sheetid) {
     // SET URL AND LINKS AND STUFF
     window.location.hash="#!/sheet:"+id;
     setTitle(data.title+' by '+data.artist);
+    
+    sheetId = id;
+    sheetTitle = data.title;
+    sheetArtist = data.artist;
+    
 }
 
 function parseChord(match, p1, offset, string) {
@@ -159,4 +171,125 @@ function parseLyrics(lyrics) {
     lyrics = lyrics.replace(/\[\[([A-Za-z0-9]*?)\]\]/gm,parseChord);
     lyrics = lyrics.replace("  "," &nbsp;");
     return lyrics;
+}
+
+
+
+/*****************************
+    COLLECTION HANDLING
+*****************************/
+
+var collection = {
+    "104184": {
+        "title":"Greatest Hits",
+        "112351235":{
+            "title":"Wonderwall",
+            "artist":"Oasis"
+        },
+        "253215532":{
+            "title":"Boulevard of Broken Dreams",
+            "artist":"Green Day"
+        },
+        "353152353":{
+            "title":"Sandstorm",
+            "artist":"Darude"
+        },
+        "1123511234":{
+            "title":"Wonderwall",
+            "artist":"Oasis"
+        },
+        "253211234":{
+            "title":"Boulevard of Broken Dreams",
+            "artist":"Green Day"
+        },
+        "353643266":{
+            "title":"Sandstorm",
+            "artist":"Darude"
+        }
+    },
+    "213512": {
+        "title":"UW Music Club",
+        "151235":{
+            "title":"Wonderwall",
+            "artist":"Oasis"
+        },
+        "2532":{
+            "title":"Boulevard of Broken Dreams",
+            "artist":"Green Day"
+        },
+        "35353":{
+            "title":"Sandstorm",
+            "artist":"Darude"
+        }
+    }
+};
+
+var sheetId = 0;
+var sheetTitle = "";
+var sheetArtist = "";
+
+function addSheet() {
+    listeningToAdd = sheetId;
+    listeningToAddTitle = sheetTitle;
+    listeningToAddArtist = sheetArtist;
+    showFolders();
+    showFiles();
+}
+
+var listeningToAdd = 0;
+var listeningToAddTitle = "";
+var listeningToAddArtist = "";
+
+function showStack(id) {
+    $("#contentslist").html("");
+    for(i in collection) {
+        if(i == id) {
+            if(listeningToAdd > 0) {
+                // AJAX STUFF GOES HERE
+                collection[i][listeningToAdd] = {
+                    "title": listeningToAddTitle,
+                    "artist": listeningToAddArtist
+                };
+            }
+            $("#folder-"+i).addClass("active");
+            var count = 0;
+            for(j in collection[id]) {
+                sheet = collection[id][j];
+                if(j != "title") {
+                    $("#contentslist").append('<div class="searchresult"><h2><a href="javascript: showSheet('+j+',1);">'+sheet.title+' <em>by '+sheet.artist+'</em></a></h2></div>');
+                    count++;
+                }
+            }
+            if(count == 0)
+                $("#contentslist").append('<div class="sorry"><h2>Oops!</h2>no results found</div>');
+            window.location.hash="#!/stack:"+id;
+            setTitle(collection[i].title);
+            $("#folderkik").css('top', ($("#folder-"+id).offset().top+12)+'px');
+            $("#folderkik").removeClass("hidden");
+        } else {
+            $("#folder-"+i).removeClass("active");
+        }
+    }
+    hideFolders();
+}
+
+function buildStacks() {
+    $("#folderthing").html("");
+    for(i in collection) {
+        stack = collection[i];
+        $("#folderthing").append('<div id="folder-'+i+'" class="folder">'+
+                                 '<a class="kik-button kik-color-lg"></a><a href="javascript: showStack('+i+');">'+stack.title+'</a>'+
+                                 '</div>');
+    }
+}
+
+function newStack(name) {
+    id = Math.floor(Math.random()*999)+10;
+    collection[id] = {
+        "title":name
+    };
+    buildStacks();
+    showStack(id);
+    $("#newstackinput").blur();
+    return false;
 }
