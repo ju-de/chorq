d0161ad444888817f8a07d6dae6f8753c732fc07
Judy Chen <time.fly.arrow@gmail.com>
2015-01-31 19:50:24 -0500

    call db from frontend

--- database/app.js
+++ database/app.js
@@ -26,6 +26,12 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+//make db accessible to router
+app.use(function(req, res, next){
+    req.db = db;
+    next();
+});
+
 app.use('/', routes);
 app.use('/users', users);
 
@@ -59,6 +65,5 @@ app.use(function(err, req, res, next) {
         error: {}
     });
 });
-
 
 module.exports = app;
--- database/routes/index.js
+++ database/routes/index.js
@@ -3,7 +3,44 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Chorq' });
+});
+
+router.get('/addsong', function(req, res) {
+    res.render('addsong', { title: 'Add Song' });
+});
+
+router.get('/songlist', function(req, res){
+	var db = req.db;
+	var collection = db.get('usercollection');
+	collection.find({}, {}, function(e, docs){
+		res.render('songlist',{
+			"songlist" :docs
+		});
+	});
+});
+
+router.post('/addsong', function(req, res){
+	var db = req.db;
+
+	//get form values
+	var title = req.body.sheetTitle;
+	var artist = req.body.sheetArtist;
+
+	var collection = db.get('usercollection');
+
+	//insert into db
+	collection.insert({
+		"sheetTitle": title,
+		"sheetArtist": artist
+	}, function(err, doc){
+		if(err){
+			res.send("Good job. You fucked up somehow.");
+		} else{
+			res.location("songlist");
+			res.redirect("songlist");
+		}
+	});
 });
 
 module.exports = router;
